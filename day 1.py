843a66a0b4247c87b23eb0b0bfd9ea193d124982
최나영 <121682792+rxmxntic@users.noreply.github.com>
2023-02-06 16:31:44 +0000

    day 1

---
+++ day 1.py
@@ -0,0 +1,12 @@
+# 1. 문자열
+
+print("Hello" + "Nayoung")
+
+# 2. 디버깅 연습
+
+# Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
--- main.py
+++ main.py
@@ -1,1 +1,2 @@
-print("Hello World!")
+print("Hello World!\nHello World!")
+print("Hello" + "Nayoung")
